78b029c51a5f99e7c7a2ecd1e5700ed1b1dd2dd5
aydcereal <jordyfigueroa93@icloud.com>
2024-02-05 06:31:15 -0800

    Refactor App.js to include input field and button for adding goals

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.buttonContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 40,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    matginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#CCCCCC",
+    flex: 1,
+  },
+  textInput: {
+    borderColor: "#CCCCCC",
+    borderWidth: 1,
+    marginRight: 8,
+    padding: 5,
+    width: "80%",
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
